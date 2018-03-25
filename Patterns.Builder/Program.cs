5af3c97adbcdf924ef1317f898e0d871be2ed66d
dopecatt <kociecki@wp.pl>
2018-03-25 18:20:56 +0200

    add Dependency Inversion Priciple project
    add Dependency Inversion Priciple project

---
+++ Patterns.Builder/Program.cs
@@ -0,0 +1,26 @@
+﻿// When piecewise object construction is complicated,
+// provide an API for doing it.
+
+using System.Collections.Generic;
+
+namespace Patterns.Builder
+{
+    public class HtmlElement
+    {
+        public string Name, Text;
+        public List<HtmlElement> Elemensts = new List<HtmlElement>();
+        private const int indentSize = 2;
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name;
+            Text = text;
+        }
+    }
+    class Program
+    {
+        static void Demo(string[] args)
+        {
+        }
+    }
+}
---
+++ Solid.DependencyInversionPriciple/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Solid.DependencyInversionPriciple
+{
+    public enum Relationship
+    {
+        Parent,
+        Child,
+        Sibling
+    }
+    public class Person
+    {
+        public string Name;
+    }
+    // low-level
+    public class Relationships : IRelationshipsBrowser
+    {
+        private List<(Person, Relationship, Person)> relations =
+            new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            relations.Add((parent, Relationship.Parent, child));
+            relations.Add((child, Relationship.Child, parent));
+        }
+
+        public IEnumerable<Person> FindAllChildrenOfParent(string name)
+        {
+            return relations
+                .Where(x => x.Item1.Name == name && x.Item2 == Relationship.Parent)
+                .Select(r => r.Item3);
+        }
+    }
+    public interface IRelationshipsBrowser
+    {
+        IEnumerable<Person> FindAllChildrenOfParent(string name);
+    }
+    class Research
+    {
+
+        static void Main(string[] args)
+        {
+            var parent = new Person() { Name = "Mark" };
+            var child1 = new Person() { Name = "Paul" };
+            var child2 = new Person() { Name = "Ann" };
+
+            Relationships _relationships = new Relationships();
+
+            _relationships.AddParentAndChild(parent, child1);
+            _relationships.AddParentAndChild(parent, child2);
+
+            _relationships.FindAllChildrenOfParent("Mark")
+        }
+    }
+}
