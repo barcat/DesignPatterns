1b43b9b1ed02d3f8b4b5fe965a4284ec28ee810f
dopecatt <kociecki@wp.pl>
2018-03-25 20:43:15 +0200

    Add Builder Pattern

--- Patterns.Builder/Program.cs
+++ Patterns.Builder/Program.cs
@@ -2,6 +2,8 @@
 // provide an API for doing it.
 
 using System.Collections.Generic;
+using System.Text;
+using System;
 
 namespace Patterns.Builder
 {
@@ -11,16 +13,80 @@ namespace Patterns.Builder
         public List<HtmlElement> Elemensts = new List<HtmlElement>();
         private const int indentSize = 2;
 
+        public HtmlElement() {
+        }
+
         public HtmlElement(string name, string text)
         {
-            Name = name;
-            Text = text;
+            Name = name ?? throw new AggregateException(nameof(name));
+            Text = text ?? throw new AggregateException(nameof(text));
+        }
+        private string ToString(int indent)
+        {
+            var stringBuilder = new StringBuilder();
+            var i = new string(' ', indentSize * indent);
+            stringBuilder.AppendLine($"{i}<{Name}>");
+
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                stringBuilder.Append(new string(' ', indentSize * (indent + 1)));
+                stringBuilder.AppendLine(Text);
+            }
+
+            foreach (var element in Elemensts)
+            {
+                stringBuilder.Append(element.ToString(indent + 1));
+            }
+            stringBuilder.AppendLine($"{i}</{Name}>");
+            return stringBuilder.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToString(0);
         }
     }
-    class Program
+
+    public class HtmlBuilder
     {
-        static void Demo(string[] args)
+        private readonly string _rootName;
+        HtmlElement root = new HtmlElement();
+        public HtmlBuilder(string rootName)
         {
+            _rootName = rootName;
+            root.Name = rootName;
+        }
+        public void AddChild(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            root.Elemensts.Add(e);   
+        }
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+    }
+
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            //var htmlBuilder = new HtmlBuilder("ul");
+            //htmlBuilder.AddChild("li", "Apple");
+            //htmlBuilder.AddChild("li", "Banana");
+            //Console.WriteLine(htmlBuilder.ToString());
+            //Console.ReadKey();
+            var sqlUpdateBuilder = new SqlUpdateBuilder("dbo.Persons");
+
+            sqlUpdateBuilder.AddValueToBeSet("name", "Ann");
+            sqlUpdateBuilder.AddValueToBeSet("sureName", "Ann");
+            sqlUpdateBuilder.AddValueToBeSet("age", "29");
+
+            sqlUpdateBuilder.AddCondition("country", "Poland");
+            sqlUpdateBuilder.AddCondition("email", "ann@test.pl");
+
+            Console.WriteLine(sqlUpdateBuilder.ToString());
+            Console.ReadKey();
         }
     }
 }
---
+++ Patterns.Builder/SqlUpdateBuilder.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Builder
+{
+
+    public class SqlUpdate
+    {
+        public string TableName { get; set; }
+        public List<(string, string)> ValuesToBeSet = new List<(string, string)>();
+        public List<(string, string)> Conditions = new List<(string, string)>();
+        private StringBuilder stringBuilder = new StringBuilder();
+
+        private string ToStringImp()
+        {
+            stringBuilder.AppendLine($"UPDATE {TableName}");
+
+            if (ValuesToBeSet.Count > 0)
+            {
+                // todo: add numeric value
+                var ValuesToBeSetStrig = ValuesToBeSet
+                    .Select(x => $"{x.Item1} = '{x.Item2}'")
+                    .Aggregate((i, j) => $"{i}, {j}");
+                stringBuilder.AppendLine($"SET " + ValuesToBeSetStrig);
+            }
+
+            if (Conditions.Count > 0)
+            {
+                var ConditionsString = Conditions
+                    .Select(x => $"{x.Item1} = '{x.Item2}'")
+                    .Aggregate((i, j) => $"{i} AND {j}");
+                stringBuilder.AppendLine($"WHERE {ConditionsString}");
+            }
+            return stringBuilder.ToString();
+        }
+        public override string ToString()
+        {
+            return ToStringImp();
+        }
+    }
+
+    public class SqlUpdateBuilder
+    {
+        private readonly SqlUpdate _sqlUpdate = new SqlUpdate();
+        public SqlUpdateBuilder(string tableName)
+        {
+            _sqlUpdate.TableName = tableName;
+        }
+
+        public void AddValueToBeSet(string columName, string newValue)
+        {
+            _sqlUpdate.ValuesToBeSet.Add((columName, newValue));
+        }
+
+        public void AddCondition(string columnName, string conditionValue)
+        {
+            _sqlUpdate.Conditions.Add((columnName, conditionValue));
+        }
+
+        public override string ToString()
+        {
+            return _sqlUpdate.ToString();
+        }
+    }
+}
